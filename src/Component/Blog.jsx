1750a174d25eeeca285b3978450cb714b40fd18b
SabbirSami <smd71430@gmail.com>
2023-09-14 11:42:20 +0600

    show data on card

--- src/Component/Blog.jsx
+++ src/Component/Blog.jsx
@@ -1,5 +1,46 @@
-const Blog = () => {
-    return <div></div>;
+import PropTypes from "prop-types";
+import { Card } from "react-bootstrap";
+const Blog = ({ blog }) => {
+    console.log(blog);
+    return (
+        <>
+            <Card className="mt-4">
+                <Card.Img variant="top" src={blog.cover} />
+                <Card.Body>
+                    <div className="py-3 d-flex align-items-center justify-content-between">
+                        <div className="author d-flex align-items-center">
+                            <div style={{ width: "50px" }}>
+                                <img
+                                    src={blog.author_img}
+                                    alt=""
+                                    className="img-fluid"
+                                />
+                            </div>
+                            <div className="ps-2">
+                                <h4 className="mb-0">{blog.author}</h4>
+                                <p className="mb-0">
+                                    <small>{blog.posted_date}</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="d-flex align-items-center">
+                            <p className="text-secondary m-0 pe-2">
+                                {blog?.reading_time} min read
+                            </p>
+                            <button className="btn p-0 m-0">book</button>
+                        </div>
+                    </div>
+                    <h3>{blog.title}</h3>
+                    <div>
+                        <p>{blog.hashtags.map((hash) => hash)}</p>
+                    </div>
+                </Card.Body>
+            </Card>
+        </>
+    );
 };
 
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired,
+};
 export default Blog;
--- src/Component/Blogs.jsx
+++ src/Component/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "./Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -9,7 +10,9 @@ const Blogs = () => {
     }, []);
     return (
         <div>
-            <h1 className="bg-primary">Hello {blogs.length}</h1>
+            {blogs.map((blog) => (
+                <Blog key={blog.id} blog={blog}></Blog>
+            ))}
         </div>
     );
 };
